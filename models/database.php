1a3dc31d1666ac2f9426a3b4549204835a787732
Patrick Covre Rodrigues <patrickcovrerodrigues@MacBook-Air-de-Patrick.local>
2025-03-10 17:46:47 -0300

    fix database

--- models/database.php
+++ models/database.php
@@ -1,10 +1,10 @@
 <?php 
 
-require __DIR__ . '../vendor/autoload.php';
+require(__DIR__ . '/../vendor/autoload.php');
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(_DIR__ . '/../');
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
@@ -12,5 +12,5 @@ $db = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Nao foi possivel conectar');
+$host = mysqli_connect($host, $db, $user, $pass) or die('Nao foi possivel conectar');
 ?>
