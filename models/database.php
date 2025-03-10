cc0c3560b2c36ace833a7cff7904cde45675f4dc
Patrick Covre Rodrigues <patrickcovrerodrigues@MacBook-Air-de-Patrick.local>
2025-03-10 17:28:17 -0300

    add .env e seguranca das informacoes do database.php

--- models/database.php
+++ models/database.php
@@ -1,8 +1,16 @@
 <?php 
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '.');
-define('DB', 'gestaoescolar');
+
+require __DIR__ . '../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(_DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Nao foi possivel conectar');
 ?>
